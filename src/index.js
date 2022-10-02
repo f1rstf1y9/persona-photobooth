80ef47ba3b2b88ab77ebaca9f856c81fd36dbadf
f1rstf1y9 <f1rstf1y9@naver.com>
2022-10-03 02:03:22 +0900

    디자인 적용 및 일부 기능 수정

--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ PoseNet using p5.js
 === */
 /* eslint-disable */
 
+import { createPicker } from "picmo";
+const rootElement = document.querySelector('#picker-container');
+
+// Create the picker
+const picker = createPicker({ rootElement });
 
 // Grab elements, create settings, etc.
 var isShooting = false;
 var selected_emoji = '';
-colors = ['#FFE56F', '#C6EBEB', '#9D86BE', '#FE8575', '#EEC3D9', '#AED6DF', '#BFCFDD']
+var selected_emoji_length = 0;
+var colors = ['#FFE56F', '#C6EBEB', '#9D86BE', '#FE8575', '#EEC3D9', '#AED6DF', '#BFCFDD']
 
 const photo = document.querySelector(".photo-canvas");
 const timer = document.querySelector(".timer");
@@ -22,6 +28,9 @@ var video = document.querySelector("#video");
 var canvas = document.querySelector("#canvas");
 var ctx = canvas.getContext("2d");
 
+let current_pose;
+let poseX, poseY;
+let w;
 
 // The detected positions will be inside an array
 let poses = [];
@@ -33,16 +42,19 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
   });
 }
 
+function dist(x1, x2, y1, y2) {
+  var a = x1 - x2;
+  var b = y1 - y2;
+  return Math.sqrt( a*a + b*b );
+}
 
 //// 촬영할때마다 reset해줄 요소들
 function resetScreen() {
   selected_emoji = '';
+  selected_emoji_length = 0;
+  $('.Emoji_emoji__iKc1G').css('background-color', '#fff');
   $('.photo-canvas').hide();
   isShooting = false;
-  emojis = document.querySelectorAll('.btn-emoji');
-  emojis.forEach((element)=>{
-    element.style.background = 'none';
-  })
 }
 
 
@@ -58,53 +70,56 @@ function drawCameraIntoCanvas() {
 
   const _sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
   //// 얼굴 인식 & 촬영 가능 여부 판단 후 촬영 시작
-  if (poses.length >= 1 && isShooting == false) {
+  if (poses.length == 1 && isShooting == false) {
     isShooting = true;
     $('.main-cam').show();
     const timecheck = async () => {
-      await _sleep(1000);
-      timer.style.display = 'block';
-      timer.textContent = 3
-      await _sleep(1000);
-      timer.textContent = 2;
-      await _sleep(1000);
-      timer.textContent = 1;
-      await _sleep(1000);
-      timer.textContent = 0;
+      for (let i = 3; i != 0; i--) {
+        await _sleep(1000);
+        timer.textContent = i;
+        timer.style.display = 'block';
+      }
       photo.getContext('2d').drawImage(video, 0, 0, photo.width, photo.height);
+      
+      current_pose = poses[0].pose;
+      poseX = (current_pose.leftEye.x + current_pose.rightEye.x)/2;
+      poseY = (current_pose.leftEye.y + current_pose.rightEye.y)/2;
+      w = dist(current_pose.leftEar.x,current_pose.leftEar.y,current_pose.rightEar.x,current_pose.rightEar.y);
       let image_data_url = photo.toDataURL('image/jpeg');
-      $('.emoji-modal').fadeIn();
+      $('.emoji-picker').fadeIn();
       $('.photo-canvas').show();
 
-      //// 이모티콘 추가 대기
-      await _sleep(1000);
-      timer.textContent = 5;
-      await _sleep(1000);
-      timer.textContent = 4;
-      await _sleep(1000);
-      timer.textContent = 3;
-      await _sleep(1000);
-      timer.textContent = 2;
-      await _sleep(1000);
-      timer.textContent = 1;
-      await _sleep(1000);
-      timer.textContent = 0;
+      // 이모티콘 추가 대기
+      for (let i = 8; i != 0; i--) {
+        await _sleep(1000);
+        timer.textContent = i;
+        timer.style.display = 'block';
+      }
 
       var context = photo.getContext('2d');
       var imageObj = new Image();
       
+      // 이모티콘 붙이기
       imageObj.onload = function() {
+        context.textAlign = "center";
         context.drawImage(imageObj, 0, 0);
-        context.font = "30px Calibri";
-        context.fillText(`${selected_emoji}`, 250, 100);
-
+        context.font = `${w*8/19}px Calibri`;
+        context.fillText(`${selected_emoji}`, poseX, poseY - w/1.2);
+        console.log(w/2.2, poseX, poseY);
         var dataURL = document.querySelector(".photo-canvas").toDataURL();
-        console.log(dataURL);
 
         //// 새 창 만들기 & 추가
         const temp = document.createElement("div");
-        temp.innerHTML = `<div class="window-top" style="background: ${ colors[Math.floor((Math.random() * 7))] }"></div>
-                          <img src="${dataURL}" alt="">`
+        let n = Math.floor((Math.random() * 2))
+        temp.innerHTML = `<div class="sub-win-${n}">
+                            <div class="window-top">
+                              <img src="src/img/sub-win_btn_${n}.svg" alt="">
+                            </div>
+                            <div class="window-btm">
+                              <img src="${dataURL}" alt="">
+                              <img src="src/img/sub-win_scroll_${n}.svg" alt="">
+                            </div>
+                          </div>`
         temp.setAttribute(
           'style',
           `left: ${Math.floor(( Math.random() * (innerWidth-320)))}px; top: ${Math.floor(( Math.random() * (innerHeight-240)))}px;`,
@@ -116,8 +131,8 @@ function drawCameraIntoCanvas() {
 
       //// 다음 촬영 준비
       $('.timer').hide();
-      $('.emoji-modal').hide();
       $('.main-cam').hide();
+      $('.emoji-picker').hide();
       await _sleep(3000);
       resetScreen();
     };
@@ -142,8 +157,13 @@ function modelReady() {
 }
 
 
-//// 이모지 선택 시 배열 추가 및 배경 어둡게
-$('.btn-emoji').click(function(){
-  selected_emoji += $(this).text();
-  $(this).css('background', 'rgba(52, 52, 52, 0.7)');
+//// 이모지 선택 이벤트
+picker.addEventListener('emoji:select', event => {
+  if (selected_emoji_length < 4 && selected_emoji.includes(event.emoji) == false) {
+    selected_emoji += event.emoji;
+    selected_emoji_length += 1;
+    console.log(selected_emoji, selected_emoji_length);
+    $((`[data-emoji$=${event.emoji}]`)).css('border-radius', '0');
+    $((`[data-emoji$=${event.emoji}]`)).css('background-color', '#E8569A');
+  }
 });
